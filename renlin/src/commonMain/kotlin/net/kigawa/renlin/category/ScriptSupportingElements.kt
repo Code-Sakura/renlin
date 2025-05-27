4e909e2a1f5a55e3ad5e2aac8072f2f9a2dab6f3
pnc-t <suga.abc.gre@gmail.com>
2025-05-27 10:46:58 +0900

    update: ScriptSupportingElements → ScriptSupportingContent

--- renlin/src/commonMain/kotlin/net/kigawa/renlin/category/ScriptSupportingElements.kt
+++
@@ -1,9 +0,0 @@
-package net.kigawa.renlin.category
-
-import net.kigawa.renlin.Html
-import net.kigawa.renlin.dsl.Dsl
-
-
-interface  ScriptSupportingElements : ContentCategory {}
-interface ScriptSupportingElementsDsl<CATEGORY_DSL : ScriptSupportingElements> :
-    Dsl<CATEGORY_DSL>
